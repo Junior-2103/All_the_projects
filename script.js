1b7eb962d545b75c4411883c95c9dd6f61dfbe72
José Wilker <jjosewilker45@gmail.com>
2025-03-15 11:40:06 -0300

    segundo commit

--- script.js
+++ script.js
@@ -17,20 +17,12 @@ let ball = document.querySelector('.main .scroll_project .ball')
 window.addEventListener('scroll',()=>{
     let scroll = window.scrollY
     if (scroll >= 1350){
+        document.querySelector('html').style.filter = "grayscale(0%)"
         ball.style.transition = "0.8s"
         ball.style.background = "linear-gradient(to right,#9a11e9,#3800d1)"
         ball.style.transform = "scale(100)"
-        ball.style.filter = "grayscale(0%)"
-        document.querySelector('.main .scroll_project .content').style.filter = "grayscale(0%)"
-        document.querySelectorAll('.main .hover_project .card')[0].classList.add('active')
-        document.querySelectorAll('.main .hover_project .card')[1].classList.add('active')
-        document.querySelectorAll('.main .hover_project .card')[2].classList.add('active')
     }else{
-        document.querySelectorAll('.main .hover_project .card')[0].classList.remove('active')
-        document.querySelectorAll('.main .hover_project .card')[1].classList.remove('active')
-        document.querySelectorAll('.main .hover_project .card')[2].classList.remove('active')
-        document.querySelector('.main .scroll_project .content').style.filter = "grayscale(100%)"
-        ball.style.filter = "grayscale(100%)"
+        document.querySelector('html').style.filter = "grayscale(100%)"
         ball.style.background = "#9a11e9"
         ball.style.transition = "0.2s"
         ball.style.transform = "translateY("+ (scroll-200) + "px) scale("+ (scroll*0.001) + ")"
